d68edf6e566b867e919805934a6fec963d8eba56
andre <lautseernandre1@gmail.com>
2024-08-11 15:34:41 +0800

    updates

---
+++ src/components/portfolio/Menu.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import pic1 from "../../assets/singlesinferno.png";
+import pic2 from "../../assets/telebot.png";
+import pic3 from "../../assets/poker.webp";
+import pic4 from "../../assets/gambling.jpg";
+
+const Menu = [
+  {
+    id: 1,
+    image: pic1,
+    title: "React Native Dating App",
+    catagory: "coding",
+    desc: "Developed and coded a mobile dating app using React Native(frontend), FireBase and Nodejs(backend) using expo",
+  },
+  {
+    id: 2,
+    image: pic2,
+    title: "Telegram bot in Java",
+    catagory: "coding",
+    desc: "Developed a Telegram Bot in Java to perform essential functions, such as delivering tailored encouragement messages based on users' responses to specific prompts",
+  },
+  {
+    id: 3,
+    image: pic3,
+    title: "From fish to shark: My journey to poker mastery",
+    catagory: "writing",
+    desc: "Poker tips and tricks",
+  },
+  {
+    id: 4,
+    image: pic4,
+    title: "Sports Betting — The Hidden Dangers",
+    catagory: "writing",
+    desc: "the hidden pitfalls of sports betting",
+  },
+];
+
+export default Menu;
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import "./portfolio.css";
+import Menu from "./Menu";
 
 const Portfolio = () => {
+  const [items, setitems] = useState(Menu);
   return (
     <section className="work container section" id="work">
       <h2 className="section__title">Recent works</h2>
+
+      <div className="work__filters">
+        <span className="work__item">Everything</span>
+        <span className="work__item">Coding</span>
+        <span className="work__item">Design</span>
+        <span className="work__item">Writing</span>
+      </div>
+
+      <div className="work__container grid">
+        {items.map((elem) => {
+          const { id, image, title, catagory, desc } = elem;
+          return (
+            <div className="work__card" key={id}>
+              <div className="work__thumbnail">
+                <img src={image} alt="image thumbnail" className="work__img" />
+                <div className="work__mask"></div>
+              </div>
+              <span className="work__catagory"></span>{" "}
+              <h3 className="work__title">{title}</h3>
+              <a href="" className="work__button">
+                <i className="icon-link work__button-icon"></i>
+              </a>
+            </div>
+          );
+        })}
+      </div>
     </section>
   );
 };
